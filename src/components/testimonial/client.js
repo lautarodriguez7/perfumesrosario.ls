d8ee33e6e861414ae27c2b60f3485f70f5f8345d
lautarodriguez7 <lautarodriguez17@gmail.com>
2024-04-04 10:45:31 -0300

    try again

--- src/components/testimonial/client.js
+++ src/components/testimonial/client.js
@@ -1,8 +1,8 @@
 const client = [
-    { img: "public/img/client/cliente3.jpeg", id: 1 },
-    { img: "public/img/client/cliente2.jpeg", id: 2 },
-    { img: "public/img/client/cliente.jpeg", id: 3 },
-    { img: "public/img/client/cliente1.jpeg", id: 4 },
+    { img: "/public/img/client/cliente3.jpeg", id: 1 },
+    { img: "/img/client/cliente2.jpeg", id: 2 },
+    { img: "img/client/cliente.jpeg", id: 3 },
+    { img: "public/img/client/cliente1.jpeg", id: 4 }
 ];
 
 export default client;
