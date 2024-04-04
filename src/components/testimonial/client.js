cab25fc2dca4c29fc203e0ed050c67bb563d64e3
lautarodriguez7 <lautarodriguez17@gmail.com>
2024-04-04 10:56:51 -0300

    changing img again

--- src/components/testimonial/client.js
+++ src/components/testimonial/client.js
@@ -1,8 +1,20 @@
 const client = [
-    { img: "/public/img/client/cliente3.jpeg", id: 1 },
-    { img: "/img/client/cliente2.jpeg", id: 2 },
-    { img: "img/client/cliente.jpeg", id: 3 },
-    { img: "public/img/client/cliente1.jpeg", id: 4 }
+    {
+        img: "https://scontent.cdninstagram.com/v/t51.29350-15/425011638_384648537495690_1851131919587333840_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTcweDIwODAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=106&_nc_ohc=ufkRMn9UEhcAb7MdJRa&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzI5Njc1MDU1MDYyNjY3NTIwMw%3D%3D.2-ccb7-5&oh=00_AfBD3VI1JkXJb9kCFxp1mqw3wddz5xwJAa2madpRXRZaXA&oe=66108839&_nc_sid=10d13b",
+        id: 1
+    },
+    {
+        img: "https://scontent.cdninstagram.com/v/t51.29350-15/425519774_689134793295993_593644934487926330_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTcweDIwODAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=109&_nc_ohc=P8JqQ3jfMLEAb7XMX8Q&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzI5NzQ1NDE1NTI1NjA3MDAxMw%3D%3D.2-ccb7-5&oh=00_AfCLNzQyT6nGTLe7HUD6h_xWJCNa-Oak6dbGCASUhYG5wA&oe=66108C38&_nc_sid=10d13b",
+        id: 2
+    },
+    {
+        img: "https://scontent.cdninstagram.com/v/t51.29350-15/425631155_211262432013202_1215190731540250859_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTcweDIwODAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=110&_nc_ohc=Dj_15ugMwCcAb4g42Nx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzI5NzY0NTY0MzA3NjQxMjMyOA%3D%3D.2-ccb7-5&oh=00_AfB12geF-2Mdly8Jv5f1C0aZ3Ky8jRQMmN-w3ODquKPg9g&oe=66109429&_nc_sid=10d13b",
+        id: 3
+    },
+    {
+        img: "https://scontent.cdninstagram.com/v/t51.29350-15/427269290_940520684191884_3530457774447744690_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMTcweDIwODAuc2RyIn0&_nc_ht=scontent.cdninstagram.com&_nc_cat=100&_nc_ohc=ogQ4rmYugNQAb7RSwqD&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MzMwMjE4NTU1ODI5Njc5MDYxNw%3D%3D.2-ccb7-5&oh=00_AfCc1FgFcfvvInzDJlWYLPXTGuef5HssbvClMa_DjBxWSQ&oe=661086C5&_nc_sid=10d13b",
+        id: 4
+    }
 ];
 
 export default client;
