ae64d563ea561220beb51f9cb319969f1b94c828
lautarodriguez7 <lautarodriguez17@gmail.com>
2024-04-04 10:38:15 -0300

    cleanning code

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -1,17 +1,11 @@
 const HeroSection = () => {
     return (
-        // <div>
-        //    <img className="object-cover w-full h-full" src="/src/assets/fondo-perfumes2.png" alt="" />
-        // </div>
-           <div className="container mx-auto">
-           <div className="w-full h-64 overflow-hidden">
-             <img
-src="img/fondo-perfumes2.png"               alt="Descripción de la imagen"
-               className="object-cover w-full h-full"
-             />
+        <div className="container mx-auto">
+            <div className="w-full h-64 overflow-hidden">
+                <img src="img/fondo-perfumes2.png" alt="Descripción de la imagen" className="object-cover w-full h-full" />
             </div>
-            </div>
+        </div>
     );
-}
+};
 
 export default HeroSection;
