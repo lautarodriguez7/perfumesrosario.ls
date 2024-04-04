0d941c409b5f0810c756a4b6c240b86b6907615a
lautarodriguez7 <lautarodriguez17@gmail.com>
2024-04-04 10:28:25 -0300

    changing position img

--- src/components/testimonial/client.js
+++ src/components/testimonial/client.js
@@ -1,7 +1,7 @@
 const client = [
-    { img: "/public/img/client/cliente.jpeg", id: 1 },
-    { img: "/public/img/client/cliente2.jpeg", id: 2 },
-    { img: "/public/img/client/cliente3.jpeg", id: 3 }
+    { img: "public/img/client/cliente.jpeg", id: 1 },
+    { img: "public/img/client/cliente2.jpeg", id: 2 },
+    { img: "public/img/client/cliente3.jpeg", id: 3 }
 ];
 
 export default client;
